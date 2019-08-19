6a1a1214e43670f98f10d400a20d0c1ed4c47ca3
yms8421 <yms8421@outlook.com>
2019-08-19 20:38:16 +0300

    initial

---
+++ BilgeAdam.CardSwitcher/BilgeAdam.CardSwitcher/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BilgeAdam.CardSwitcher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Players()
+        {
+            return View();
+        }
+    }
+}
